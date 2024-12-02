7f5c3f7a8f0ad07a92a872cb1eb91119ea296237
Sr-bauducco <israel.telesbandeira@gmail.com>
2024-12-02 19:47:55 -0300

    VerificaVelha por linha - [F/S][ok]

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -11,10 +11,15 @@
 
 
 TEST_CASE( "Testa velha", "[single-file]" ) {
-	int teste_linha1[3][3]= {   { 2, 0, 1 }, 
-	                      		{ 2, 0, 1 },
-						  		{ 0, 1, 2 }
+	int teste_linha_falha[3][3]= {   { 2, 2, 0 }, 
+	                      			{ 1, 0, 1 },
+						  			{ 1, 1, 2 }
 					  };
-    REQUIRE( VerificaVelha(teste_linha1) == 1 );
+	int teste_linha_sucesso[3][3]{   { 2, 2, 2 }, 
+	                      			{ 1, 0, 1 },
+						  			{ 0, 1, 1 }
+					  };
+  	REQUIRE( VerificaVelha(teste_linha_falha) == 0 );
+	REQUIRE( VerificaVelha (teste_linha_sucesso)== 1);
 } 
  
