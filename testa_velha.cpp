c144b9aa83795061c6c6fdf254a583c6189013e9
Sr-bauducco <israel.telesbandeira@gmail.com>
2024-12-03 18:27:36 -0300

    refatoração de testes - [ok]

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -11,61 +11,36 @@
 
 
 TEST_CASE( "Testa velha", "[single-file]" ) {
-	int teste_linha_falha[3][3]= {   { 2, 1, 1 }, 
-	                      			 { 1, 2, 2 },
-						  			 { 1, 2, 1}
+
+	int teste_linha_sucesso[3][3]{   {1, 1, 1},
+    								 {2, 2, 0},
+    								 {0, 0, 0}
 					  };
-	int teste_linha_sucesso[3][3]{   { 1, 1, 1 }, 
-	                      			 { 2, 0, 2 },
-						  			 { 0, 2, 2 }
-					  };
-	int teste_coluna_falha[3][3]= {   { 2, 1, 1 }, 
-	                      			  { 1, 2, 2 },
-						  			  { 1, 2, 1}
-					  };
-	int teste_coluna_sucesso[3][3]{   { 0, 2, 1 }, 
-	                      			  { 1, 2, 2 },
-						  			  { 1, 2, 1 }
+	int teste_coluna_sucesso[3][3]{    {1, 1, 0},
+    								   {1, 2, 0},
+    								   {1, 2, 2}
 					  };
 	int teste_empate[3][3]{    {1, 2, 1},
     						   {2, 1, 2},
     						   {2, 1, 1}
 					  };
-	int teste_diagonal_falha[3][3]{   { 2, 1, 1 }, 
-	                      			  { 1, 2, 2 },
-						  			  { 1, 2, 1}
+	int teste_diagonal_sucesso[3][3]{  {1, 2, 1},  
+    								   {2, 1, 2},  
+    								   {1, 2, 1}
 					  };
-	int teste_diagonal_sucesso[3][3]{  { 2, 2, 1 }, 
-	                    	  		   { 1, 1, 2 },
-							  		   { 1, 2, 1 }
+	int teste_vazio_sucesso[3][3]{     {1, 2, 0}, 
+    								   {2, 1, 0}, 
+    								   {0, 1, 2}  
 					  };
-	int teste_vazio_falha[3][3]{       { 2, 1, 1 }, 
-	                      			   { 1, 2, 2 },
-						  			   { 1, 2, 1}
-					  };
-	int teste_vazio_sucesso[3][3]{     { 0, 0, 2 }, 
-	                    	  		   { 1, 2, 2 },
-							  		   { 1, 0, 1 }
-					  };
-	int teste_impossivel_falha[3][3]{       { 2, 1, 1 }, 
-	                      			   { 1, 2, 2 },
-						  			   { 1, 2, 1}
-					  };
-	int teste_impossivel_sucesso[3][3]{ { 1, 1, 1 }, 
-	                    	  		   	{ 1, 1, 1 },
-							  		   	{ 1, 1, 1 }
+	int teste_impossivel_sucesso[3][3]{ {1, 1, 1},
+    									{1, 1, 1},
+    									{1, 1, 1}
 					  };
 
-	
-  	REQUIRE( VerificaVelha(teste_linha_falha) == 0 );
 	REQUIRE( VerificaVelha (teste_linha_sucesso)== 1);
-	REQUIRE( VerificaVelha (teste_coluna_falha) == 0 );
-	REQUIRE( VerificaVelha (teste_coluna_sucesso) == 2);
-	REQUIRE(VerificaVelha (teste_diagonal_falha) == 0);
+	REQUIRE( VerificaVelha (teste_coluna_sucesso) == 1);
 	REQUIRE(VerificaVelha (teste_diagonal_sucesso) == 1);
-	REQUIRE(VerificaVelha (teste_vazio_falha) == 0);
 	REQUIRE(VerificaVelha (teste_vazio_sucesso) == -1);
-	REQUIRE(VerificaVelha (teste_impossivel_falha) == 0);
 	REQUIRE(VerificaVelha (teste_impossivel_sucesso) == -2);
 	REQUIRE( VerificaVelha (teste_empate) == 0);
 
