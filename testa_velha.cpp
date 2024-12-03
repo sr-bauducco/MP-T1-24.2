f476c1da96870295418d9e57a7b7e4b1333745ba
Sr-bauducco <israel.telesbandeira@gmail.com>
2024-12-03 00:27:13 -0300

    verificaVelha por coluna - [F/S][ok]

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -19,13 +19,13 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 	                      			 { 1, 0, 1 },
 						  			 { 0, 1, 1 }
 					  };
-	int teste_coluna_falha[3][3]= {   { 2, 2, 0 }, 
-	                      			  { 1, 0, 1 },
-						  			  { 1, 1, 2 }
+	int teste_coluna_falha[3][3]= {   { 2, 0, 1 }, 
+	                      			  { 1, 2, 2 },
+						  			  { 1, 2, 1 }
 					  };
-	int teste_coluna_sucesso[3][3]{   { 1, 2, 1 }, 
-	                      			  { 1, 2, 1 },
-						  			  { 0, 2, 0 }
+	int teste_coluna_sucesso[3][3]{   { 0, 2, 1 }, 
+	                      			  { 1, 2, 2 },
+						  			  { 1, 2, 1 }
 					  };
   	REQUIRE( VerificaVelha(teste_linha_falha) == 0 );
 	REQUIRE( VerificaVelha (teste_linha_sucesso)== 1);
--- velha.cpp
+++ velha.cpp
@@ -16,23 +16,30 @@
 
 int VerificaVelha( int velha[3][3] )
 {
-    // confere linha por linha para saber se houve vitoria
-    for (int i = 0; i < 3; i++) {
-        if (velha[i][0] != 0 && velha[i][0] == velha[i][1] && velha[i][1] == velha[i][2]) {
-            return 1; 
-        }else{ 
-            return 0;
-        } };
-        //confere coluna por coluna
+    // Verificando as linhas
     for (int i = 0; i < 3; ++i) {
-        if (velha[0][i] == velha[1][i] && velha[1][i] == velha[2][i] && velha[0][i] != 0) {
-            return 1;
-        }else{
-            return 0;
+        if (velha[i][0] != 0 && velha[i][0] == velha[i][1] && velha[i][1] == velha[i][2]) {
+            return 1; // Vitória na linha
         }
     }
     
+    // Verificando as colunas
+    for (int i = 0; i < 3; ++i) {
+        if (velha[0][0] == velha[1][0] && velha[1][0] == velha[2][0] && velha[0][0] != 0)  
+        {
+            return 1; // Vitória na coluna
+        }else if( velha[0][1] == velha[1][1] && velha[1][1] == velha[2][1] && velha[0][1] != 0 )
+        {
+            return 1;
+        }else if(velha[0][2] == velha[1][2] && velha[1][2] == velha[2][2] && velha[0][2] != 0){
+            return 1;
+        }
+        return 0;
+    }
+
+    return 0; // Nenhuma vitória
 };
+
 
 
 
